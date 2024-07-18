5f7d83c7115e175621cbbf0ccad8d690f5576ec3
谢彬彬 <xiebinbin@sodagreen.love>
2024-07-18 09:15:23 +0800

    接口层

--- app/api/auth.ts
+++ app/api/auth.ts
@@ -1,12 +1,7 @@
 import { createInstance } from "./req"
 
-export interface IUser {
-    id: number;
-    userName: string;
-    nickName: string;
-    gender: number;
-}
-const signUp = async (): Promise<IUser> => await createInstance(true).post('/auth/signup')
+
+const signUp = async (): Promise<Model.IUser> => await createInstance(true).post('/auth/signup')
 export default {
     signUp
 }
