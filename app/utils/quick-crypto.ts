930431ebb0a2ed3dca51beb8487641557042d666
谢彬彬 <xiebinbin@sodagreen.love>
2024-07-18 08:56:39 +0800

    加密库

--- app/utils/quick-crypto.ts
+++ app/utils/quick-crypto.ts
@@ -10,18 +10,18 @@ const En = (password: string, data: Uint8Array) => {
     const authTag = cipher.getAuthTag();
     return new Uint8Array(Buffer.concat([
         iv,
+        encrypted,
         authTag,
-        encrypted
     ]))
 }
 const De = (password: string, data: Uint8Array): Uint8Array => {
     const key = Crypto.createHash('sha256').update(password).digest();
     const iv = data.slice(0, 12);
-    const authTag = data.slice(12, 28);
-    const encrypted = data.slice(28);
+    let encrypted = data.slice(12);
+    const authTag = encrypted.slice(encrypted.length - 16, encrypted.length);
     const decipher = Crypto.createDecipheriv('aes-256-gcm', key, iv);
     decipher.setAuthTag(authTag);
-    let decrypted = decipher.update(encrypted);
+    let decrypted = decipher.update(encrypted.slice(0, encrypted.length - 16));
     decrypted = Buffer.concat([decrypted as Buffer, decipher.final() as Buffer]);
     return new Uint8Array(decrypted);
 }
