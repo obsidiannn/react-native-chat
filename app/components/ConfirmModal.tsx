a88f55fd1830e3a276dcdc15c42f3ec429539373
谢彬彬 <xiebinbin@sodagreen.love>
2024-08-05 18:51:24 +0800

    拆分 card 与新增颜色文件

--- app/components/ConfirmModal.tsx
+++ app/components/ConfirmModal.tsx
@@ -1,48 +1,24 @@
-import { forwardRef, ReactNode, useImperativeHandle, useState } from "react";
-import { ColorValue, Modal, Text, View, ViewStyle } from "react-native";
+import { forwardRef, useImperativeHandle, useState } from "react";
+import { Modal, Text, TextStyle, View, ViewStyle } from "react-native";
 import { ColorsState } from "app/stores/system";
 import { s } from "app/utils/size";
 import { useRecoilValue } from "recoil";
-import { Button } from "app/components";
+import { Button, Card } from "app/components";
 export interface ConfirmModalOption {
-    title: string;
-    content: string;
-    onSubmit?: () => void;
-    onCancel?: () => void;
+  title: string;
+  content: string;
+  onSubmit?: () => void;
+  onCancel?: () => void;
 }
-export interface ConfirmModalType {
-    open: (option: ConfirmModalOption) => void;
-}
+
 export interface ConfirmModalType {
   open: (option: ConfirmModalOption) => void,
   close: () => void
 }
-const Card = (props: {
-  rounded?: boolean,
-  backgroundColor?: ColorValue,
-  children: ReactNode,
-  style?: ViewStyle
-}) => {
-  const $colors = useRecoilValue(ColorsState);
-  return <View style={[
-    {
-      width: "100%",
-      backgroundColor: props.backgroundColor ?? $colors.background,
-      marginBottom: s(10),
-      padding: s(16),
-    },
-    props.rounded && {
-      borderRadius: s(20)
-    },
-    props.style
-  ]}>
-    {props.children}
-  </View>
-}
 export const ConfirmModal = forwardRef((_, ref) => {
   const [visible, setVisible] = useState(false);
   const $colors = useRecoilValue(ColorsState);
-  const [option,setOption]=useState<ConfirmModalOption>({
+  const [option, setOption] = useState<ConfirmModalOption>({
     title: "",
     content: "",
     onSubmit: () => { },
@@ -50,49 +26,52 @@ export const ConfirmModal = forwardRef((_, ref) => {
   })
   useImperativeHandle(ref, () => ({
     open: (option: ConfirmModalOption) => {
-        setVisible(true);
-        setOption(option)
+      setVisible(true);
+      setOption(option)
     },
     close: async () => setVisible(false)
   }));
   return (
-    <Modal transparent={true} style={{ flex: 1}} visible={visible} animationType="slide" >
-      <View style={{
-        flex: 1,
-        backgroundColor: "#000000cc",
-        alignItems:"center",
-        paddingHorizontal:s(16),
-        justifyContent:"center"
-      }}>
+    <Modal transparent={true} style={{ flex: 1 }} visible={visible} animationType="slide" >
+      <View style={$container}>
         <Card rounded>
-          <View style={{
-            marginVertical: s(16)
-          }}>
-            <Text style={{
-              fontSize:s(16),
-              color: $colors.text,
-              fontWeight:"500",
-              textAlign:"center"
-            }}>{option.title}</Text>
+          <View style={$titleContainer}>
+            <Text style={[$title,{color: $colors.text}]}>{option.title}</Text>
           </View>
-          <Text style={{
-            fontSize: s(14),
-            color: $colors.secondaryText,
-            marginHorizontal:s(15)
-          }}>{option.content}</Text>
+          <Text style={[$content,{color: $colors.secondaryText}]}>{option.content}</Text>
           <View>
             <Button onPress={async () => {
-                option.onSubmit?.();
-                setVisible(false);
+              option.onSubmit?.();
+              setVisible(false);
             }} containerStyle={{
               marginTop: s(15)
-            }} rounded fullRounded size="large" label="确认"/>
+            }} rounded fullRounded size="large" label="确认" />
             <Button onPress={() => setVisible(false)} containerStyle={{
               marginVertical: s(15)
-            }} rounded fullRounded type="secondary" size="large" label="取消"/>
+            }} rounded fullRounded type="secondary" size="large" label="取消" />
           </View>
         </Card>
       </View>
     </Modal>
   )
 })
+
+const $container: ViewStyle = {
+  flex: 1,
+  backgroundColor: "#000000cc",
+  alignItems: "center",
+  paddingHorizontal: s(16),
+  justifyContent: "center"
+}
+const $titleContainer: ViewStyle = {
+  marginVertical: s(16)
+}
+const $title: TextStyle = {
+  fontSize: s(16),
+  fontWeight: "500",
+  textAlign: "center"
+}
+const $content: TextStyle = {
+  fontSize: s(14),
+  marginHorizontal: s(15)
+}
